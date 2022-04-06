2a3a8ba834a013a149ccde49d5d2c6aed0c0975a
Danilo Henrique <daniloohenriquee@gmail.com>
2022-04-06 00:08:32 -0300

    First commit, 3 tests de 5

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,46 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  console.log(tasks);
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle != '') {
+      const id: number = Math.floor(Math.random() * (1000 - 1) + 1);
+      const data = [...tasks, {id: id, title: newTaskTitle, isComplete: false}];
+      setTasks(data);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let trueOrFalse: boolean;
+
+    const data = tasks.map( (arr) => {
+      if(arr.id === id){
+        return {id: arr.id, title: arr.title, isComplete: !arr.isComplete};
+      } else {
+        return arr;
+      }
+    });
+    setTasks(data);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    console.log("id do botao excluir: " + id);
+
+    for(let i = 0; i < tasks.length; i++) {
+      if(tasks[i].id === id) {
+
+        console.log(tasks);
+
+        tasks.splice(i, 1);
+        // console.log(newTask);
+        console.log(tasks); 
+        // setTasks(tasks);
+      }
+    }
   }
 
   return (
