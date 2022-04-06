9cb4a72954a268bcc68d23e7eeec9b39d6cdc448
Danilo Henrique <daniloohenriquee@gmail.com>
2022-04-06 20:38:50 -0300

    adicionado arquivo de correcao do desafio

--- src/components/TaskListCorrecao.tsx
+++ src/components/TaskListCorrecao.tsx
@@ -16,37 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    if(newTaskTitle != '') {
-      const id: number = Math.floor(Math.random() * (1000 - 1) + 1);
-      const data = [...tasks, {id: id, title: newTaskTitle, isComplete: false}];
-      setTasks(data);
-      setNewTaskTitle('');
+    if(!newTaskTitle) return;
+    const newTask =  {
+      id: Math.floor(Math.random() * (1000 - 1) + 1),
+      title: newTaskTitle,
+      isComplete: false
     }
+      setTasks( (oldTasks) => {
+        return [...oldTasks, newTask]
+      });
+
+      setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const data = tasks.map( (arr) => {
-      if(arr.id === id){
-        return {id: arr.id, title: arr.title, isComplete: !arr.isComplete};
-      } else {
-        return arr;
-      }
+    const newTasks = tasks.map( (task) => {
+      return task.id === id ? { ...task, isComplete: !task.isComplete } : task
     });
-    setTasks(data);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
-    for(let i = 0; i < tasks.length; i++) {
-      if(tasks[i].id === id) {
-
-        let array = [...tasks];
-        array.splice(i, 1);
-
-        setTasks(array);
-      }
-    }
+    const filteredTasks = tasks.filter( task => task.id != id);
+    setTasks(filteredTasks);
   }
 
   return (
